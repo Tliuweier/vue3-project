7813e02da68a864edf3c74b0da7d8eef6568ece7
tanjinwei <374055608@qq.com>
2023-05-08 19:02:49 +0800

    删除dist文件

--- dist/assets/index-1554bf0f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-f18de0af.js";const c={};function n(r,t){return Vue.openBlock(),Vue.createElementBlock("div",null,"我是马车")}const _=e(c,[["render",n]]);export{_ as default};
--- dist/assets/index-6141c7dd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-f18de0af.js";const c={};function n(r,t){return Vue.openBlock(),Vue.createElementBlock("div",null,"关于我")}const _=e(c,[["render",n]]);export{_ as default};
