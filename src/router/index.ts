12f7fb61f172af39018c9386d54274c9105b94b7
tanjinwei <374055608@qq.com>
2023-05-10 19:38:50 +0800

    修改路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+// import indexVue from '../views/index.vue'
 const pageModules = import.meta.glob('../views/**/page.ts',{
     eager:true,
     import:'default'
 })
 const comModules = import.meta.glob('../views/**/index.vue')
 // console.log(comModules)
-const routes:any = Object.entries(pageModules).map(([pagePath,config])=>{
+let  routes:any = Object.entries(pageModules).map(([pagePath,config])=>{
     // console.log(pagePath,config)
     let path = pagePath.replace('../views','').replace('/page.ts','')
-    path = path || '/'
+    path = path || '/index'
     let name = path.split('/').filter(Boolean).join('-') || 'index'
     let comPath = pagePath.replace('page.ts','index.vue')
-    // console.log(comPath)
+    console.log(`/vue3-project${path}`)
     return {
-		path,
+		path:`/vue3-project${path}`,
 		name,
 		component:comModules[comPath],
 		meta:config
@@ -22,6 +22,14 @@ const routes:any = Object.entries(pageModules).map(([pagePath,config])=>{
 })
 console.log(routes)
 
+// let routes1 = [
+//     {
+//         path:'/vue3-project/',
+//         name:'index',
+//         component:()=>indexVue,
+//     },
+
+// ]
 const router = createRouter({
     history:createWebHistory(),
     routes
