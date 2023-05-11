6b59bbd14703c3cc9c683c8b199cd5502cab96cf
tanjinwei <374055608@qq.com>
2023-05-11 14:20:36 +0800

    删除dist

--- dist/assets/index-cc5b814a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-e7ade9f7.js";import"vue";const c={};function n(r,t){return Vue.openBlock(),Vue.createElementBlock("div",null,"关于我")}const s=e(c,[["render",n]]);export{s as default};
